0bef2eb0cdc812643e890b8f05cee9b7481fe523
Jeetendra Salgaonkar <jeetendra-atul.salgaonkar@sixt.com>
2024-12-02 00:58:00 +0100

    [AB-0000]: Added setup file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa_eligibility_check",
+    version="0.0.0",
+    author="Jeetendra Salgaonkar",
+    author_email="jeetendra.salgaonkar@gmail.com",
+    packages=find_packages()
+)
