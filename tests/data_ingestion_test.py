3c0dfd0a1233b8b0e2c1bb7ee7d2667f6bb46ce3
Jeetendra Salgaonkar <jeetendra-atul.salgaonkar@sixt.com>
2024-12-09 12:42:21 +0100

    [AB-0000]: Added a negative test.

--- tests/data_ingestion_test.py
+++ tests/data_ingestion_test.py
@@ -6,11 +6,17 @@ from com.demo.visa.constants import FEATURE_STORE_EXPORT_FILE_NAME, DATA_INGESTI
     DATA_INGESTION_DIRECTORY_NAME, DATA_INGESTION_GENERATED_DIRECTORY, TRAINING_FILE_NAME, TESTING_FILE_NAME, \
     DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO, DATA_INGESTION_TABLE_NAME
 from com.demo.visa.entity.configuration_entity import DataIngestionConfigurationEntity
+from com.demo.visa.exception import VisaException
 
 
 class MyTestCase(unittest.TestCase):
 
-    def __init__(self):
+    @staticmethod
+    def assert_is_file(path):
+        if not os.path.isfile(path):
+            raise AssertionError("File does not exist: %s" % str(path))
+
+    def test_export_data_from_database_into_csv_successful_test(self):
         data_ingestion_configuration_entity = DataIngestionConfigurationEntity(
             data_ingestion_directory=os.path.join(DATA_INGESTION_DIRECTORY_NAME),
             data_ingestion_feature_store_directory=os.path.join(DATA_INGESTION_FEATURE_STORE_DIRECTORY),
@@ -18,16 +24,23 @@ class MyTestCase(unittest.TestCase):
             data_ingestion_testing_file_location=os.path.join(DATA_INGESTION_GENERATED_DIRECTORY, TESTING_FILE_NAME),
             data_ingestion_train_test_split_ratio=DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
         )
-
-    @staticmethod
-    def assert_is_file(path):
-        if not os.path.isfile(path):
-            raise AssertionError("File does not exist: %s" % str(path))
-
-    def test_export_data_from_database_into_csv(self):
-        DataIngestion(self.data_ingestion_configuration_entity).export_data_from_database_into_csv()
+        DataIngestion(data_ingestion_configuration_entity).export_data_from_database_into_csv()
         path = os.path.join(DATA_INGESTION_FEATURE_STORE_DIRECTORY, FEATURE_STORE_EXPORT_FILE_NAME)
         self.assert_is_file(path)
+
+    # Show throw exception
+    def test_export_data_from_database_into_csv_empty_feature_dir_param(self):
+        data_ingestion_configuration_entity_empty_feature_dir = DataIngestionConfigurationEntity(
+            data_ingestion_directory=os.path.join(DATA_INGESTION_DIRECTORY_NAME),
+            data_ingestion_feature_store_directory=os.path.join(''),
+            data_ingestion_training_file_location=os.path.join(DATA_INGESTION_GENERATED_DIRECTORY, TRAINING_FILE_NAME),
+            data_ingestion_testing_file_location=os.path.join(DATA_INGESTION_GENERATED_DIRECTORY, TESTING_FILE_NAME),
+            data_ingestion_train_test_split_ratio=DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+        )
+        data_ingestion: DataIngestion = DataIngestion(data_ingestion_configuration_entity_empty_feature_dir)
+        with self.assertRaises(VisaException) as context:
+            data_ingestion.export_data_from_database_into_csv()
+        self.assertTrue('No such file or directory' in context.exception.error_message)
 
 
 if __name__ == '__main__':
